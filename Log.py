7d5d60240f02940d89ac65e1ae0318ea4ff144a0
Dan Herlihy <djherls@gmail.com>
2017-05-03 17:29:26 -0400

    Logging issue

--- Log.py
+++ Log.py
@@ -1,11 +1,16 @@
 import re
+from pprint import pprint
 
 
 def err(spreadsheet=None, error="", raw=False):
-    if raw:
-        m = re.search("\".+?: (.+)\"", str(error))
-        error = m.group(1)
-    if spreadsheet:
-        print("\nERROR in {0}({1}): {2}".format(spreadsheet['name'], spreadsheet['id'], error))
-    else:
-        print("\nERROR: {0}".format(error))
+    try:
+        if raw:
+            m = re.search("\".+?: (.+)\"", str(error))
+            error = m.group(1)
+        if spreadsheet:
+            print("\nERROR in {0}({1}): {2}".format(spreadsheet['name'], spreadsheet['id'], error))
+        else:
+            print("\nERROR: {0}".format(error))
+    except AttributeError:
+        pprint(spreadsheet)
+        print(error)
